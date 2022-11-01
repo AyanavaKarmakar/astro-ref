d2af30aea46faa74797d85816a34b1a6586c038a
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2022-11-01 17:44:08 +0530

    feat(components): Greeting component using preact

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,15 @@
+import { useState } from "preact/hooks";
+
+export const Greeting = ({ messages }) => {
+  const randomMessage = () =>
+    messages[Math.floor(Math.random() * messages.length)];
+
+  const [greeting, setGreeting] = useState(randomMessage());
+
+  return (
+    <div>
+      <h3>{greeting}! Thank you for visiting!</h3>
+      <button onClick={() => setGreeting(randomMessage())}>New Greeting</button>
+    </div>
+  );
+};
